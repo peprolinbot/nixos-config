a589e94805397034e65ef35b33e7858100278c4f
Pedro Rey Anca <contact@peprolinbot.com>
2024-07-09 09:53:44 +0200

    Add nextcloud-client

--- modules/home/default.nix
+++ modules/home/default.nix
@@ -17,6 +17,7 @@
     ++ [(import ./hyprland)] # window manager
     ++ [(import ./kitty.nix)] # terminal
     ++ [(import ./swaync/swaync.nix)] # notification deamon
+    ++ [(import ./nextcloud.nix)] # nextcloud client
     ++ [(import ./nvim.nix)] # neovim editor
     ++ [(import ./packages.nix)] # other packages
     ++ [(import ./retroarch.nix)]
