3b2d7e021d3fb5e9a5b9ac9ebb4679cf3cea91d4
Pedro Rey Anca <personal@peprolinbot.com>
2024-07-21 13:11:15 +0200

    Update Regreet config

--- modules/core/wayland.nix
+++ modules/core/wayland.nix
@@ -22,8 +22,12 @@
   programs.regreet = {
     enable = true;
     settings = {
-      background.path = pkgs.nixos-artwork.wallpapers.simple-dark-gray-bottom.gnomeFilePath;
+      background = {
+        path = pkgs.nixos-artwork.wallpapers.simple-dark-gray-bottom.gnomeFilePath;
+        fit = "Cover";
+      };
     };
+    cageArgs = ["-s" "-m" "last"]; # Only show on the last monitor
   };
 
   environment.sessionVariables.XKB_DEFAULT_LAYOUT = "es";
