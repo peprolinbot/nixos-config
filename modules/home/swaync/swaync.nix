3fc6414afa0b1d46752d921797962cc438730396
Pedro Rey Anca <personal@peprolinbot.com>
2024-07-29 14:09:55 +0200

    Change swaync menu

--- modules/home/swaync/swaync.nix
+++ modules/home/swaync/swaync.nix
@@ -1,5 +1,5 @@
 {pkgs, ...}: {
-  home.packages = with pkgs; [swaynotificationcenter];
+  home.packages = with pkgs; [swaynotificationcenter kooha];
   xdg.configFile."swaync/style.css".source = ./style.css;
   xdg.configFile."swaync/config.json".source = ./config.json;
 }
