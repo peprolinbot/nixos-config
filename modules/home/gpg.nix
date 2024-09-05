3f7ed9bfddeb24504492000cfb10e2023df8b0b1
Pedro Rey Anca <personal@peprolinbot.com>
2024-09-05 11:55:32 +0200

    Fix gpg-agent

--- modules/home/gpg.nix
+++ modules/home/gpg.nix
@@ -1,7 +1,9 @@
-{...}: {
+{pkgs, ...}: {
+  programs.gpg.enable = true;
   services.gpg-agent = {
     enable = true;
     enableSshSupport = true;
+    pinentryPackage = pkgs.pinentry-gnome3;
     sshKeys = ["D2A3680589B81F2B07DA6CFE21DF848F1AF4E9D3"];
   };
 }
