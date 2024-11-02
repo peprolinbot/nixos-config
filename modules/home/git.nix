aa2aa0ef2d814709e6f09281c657b487b6e88dc7
Pedro Rey Anca <personal@peprolinbot.com>
2024-11-02 16:48:29 +0100

    Sign git commits

--- modules/home/git.nix
+++ modules/home/git.nix
@@ -2,8 +2,13 @@
   programs.git = {
     enable = true;
 
-    userName = "peprolinbot";
+    userName = "Pedro Rey Anca";
     userEmail = "personal@peprolinbot.com";
+
+    signing = {
+      signByDefault = true;
+      key = null; # Let GnuPG decide what signing key to use depending on commit’s author.
+    };
 
     extraConfig = {
       init.defaultBranch = "main";
