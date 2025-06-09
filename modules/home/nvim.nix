5bf8070ee0f6d76845111440d52c140c1002ea44
Pedro Rey Anca <personal@peprolinbot.com>
2025-06-09 19:05:14 +0200

    LunarVim all the way

--- modules/home/nvim.nix
+++ modules/home/nvim.nix
@@ -1,9 +1,18 @@
 {pkgs, ...}: {
-  programs.neovim = {
+  home.packages = with pkgs; [lunarvim];
+
+  programs.neovide = {
     enable = true;
-    vimAlias = true;
+    settings = {
+      neovim-bin = "${pkgs.lunarvim}/bin/lvim";
+    };
   };
-  home.packages = [pkgs.lunarvim];
+
+  home.shellAliases = builtins.listToAttrs (map (key: {
+      name = key;
+      value = "lvim";
+    })
+    ["vi" "vim" "nvim"]);
 
   home.sessionPath = ["${pkgs.clang-tools}/bin:$PATH"];
 }
