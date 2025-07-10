b837ef9a8d0fb923df31a019b4ffe727a7c194c1
Pedro Rey Anca <personal@peprolinbot.com>
2025-07-10 12:00:48 +0200

    Disable grub theming

--- modules/core/stylix.nix
+++ modules/core/stylix.nix
@@ -7,6 +7,9 @@
   stylix = {
     enable = true;
     base16Scheme = "${pkgs.base16-schemes}/share/themes/catppuccin-mocha.yaml";
-    targets.console.enable = false;
+    targets = {
+      console.enable = false;
+      grub.enable = false;
+    };
   };
 }
