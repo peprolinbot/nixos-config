a3123cfdec54a04c671153de257c419a2d0b7fc6
Pedro Rey Anca <personal@peprolinbot.com>
2025-08-19 13:20:35 +0200

    Move networking stuff to separate file

--- machines/aresix/configuration.nix
+++ machines/aresix/configuration.nix
@@ -3,27 +3,11 @@
     ./modules/reverse-proxy.nix
     ./modules/home-assistant.nix
     ./modules/dyndns.nix
+    ./modules/network.nix
   ];
 
   services.logind.lidSwitch = "ignore";
   boot.kernelParams = ["consoleblank=60"]; # Blanks console (screen off) after 60s
-
-  networking = {
-    interfaces = {
-      enp1s0.ipv4.addresses = [
-        {
-          address = "192.168.1.30";
-          prefixLength = 24;
-        }
-      ];
-    };
-
-    defaultGateway = {
-      address = "192.168.1.1";
-      interface = "enp1s0";
-    };
-    nameservers = ["1.1.1.1" "8.8.8.8"];
-  };
 
   system.stateVersion = "25.05";
 }
---
+++ machines/aresix/modules/network.nix
@@ -0,0 +1,18 @@
+{...}: {
+  networking = {
+    interfaces = {
+      enp1s0.ipv4.addresses = [
+        {
+          address = "192.168.1.30";
+          prefixLength = 24;
+        }
+      ];
+    };
+
+    defaultGateway = {
+      address = "192.168.1.1";
+      interface = "enp1s0";
+    };
+    nameservers = ["1.1.1.1" "8.8.8.8"];
+  };
+}
