5793c9096aff78f3d017689dd5c4c69f296f4a42
Pedro Rey Anca <personal@peprolinbot.com>
2025-09-08 19:00:41 +0200

    machine aresix: add ecryptfs (request by juan)

--- machines/aresix/modules/users.nix
+++ machines/aresix/modules/users.nix
@@ -1,8 +1,13 @@
-{...}: {
+{pkgs, ...}: {
   users.users.juan = {
     isNormalUser = true;
     openssh.authorizedKeys.keys = [
       "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAIFpkZoYCFS6jQyaLgRkG8WlOj8ybpwsJkCWTuKkGB5oA Juan Rey"
     ];
   };
+
+  # Requested by juan
+  environment.systemPackages = [pkgs.ecryptfs];
+  programs.ecryptfs.enable = true;
+  boot.kernelModules = ["ecryptfs"];
 }
