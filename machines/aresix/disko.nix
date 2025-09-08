c3c618a98b8cfe5b0bad8ae397db014ab0759876
Pedro Rey Anca <personal@peprolinbot.com>
2025-09-08 19:01:00 +0200

    format: apply alejandra

--- machines/aresix/disko.nix
+++ machines/aresix/disko.nix
@@ -1,12 +1,11 @@
 # ---
 # schema = "single-disk"
 # [placeholders]
-# mainDisk = "/dev/disk/by-id/ata-TOSHIBA_MQ01ABD100_17B5PEO5T" 
+# mainDisk = "/dev/disk/by-id/ata-TOSHIBA_MQ01ABD100_17B5PEO5T"
 # ---
 # This file was automatically generated!
 # CHANGING this configuration requires wiping and reinstalling the machine
 {
-
   boot.loader.grub.efiSupport = true;
   boot.loader.grub.efiInstallAsRemovable = true;
   boot.loader.grub.enable = true;
@@ -31,7 +30,7 @@
                 type = "filesystem";
                 format = "vfat";
                 mountpoint = "/boot";
-                mountOptions = [ "umask=0077" ];
+                mountOptions = ["umask=0077"];
               };
             };
             root = {
