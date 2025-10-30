715f3d6f936b47c927fc3bc00b03b22981d0882b
Pedro Rey Anca <personal@peprolinbot.com>
2025-10-30 19:11:35 +0100

    Add JSON config for Helix

--- users/pedro/home-configuration/helix.nix
+++ users/pedro/home-configuration/helix.nix
@@ -1,4 +1,5 @@
-{pkgs, ...}: {
+{ pkgs, ... }:
+{
   programs.helix = {
     enable = true;
     defaultEditor = true;
@@ -6,32 +7,49 @@
       language-server = {
         pyright = {
           command = "${pkgs.pyright}/bin/pyright-langserver";
-          args = ["--stdio"];
-          config = {};
+          args = [ "--stdio" ];
+          config = { };
         };
 
-        nil = {command = "${pkgs.nil}/bin/nil";};
+        nil = {
+          command = "${pkgs.nil}/bin/nil";
+        };
+
+        vscode-json-language-server.command = "${pkgs.nodePackages.vscode-json-languageserver}/bin/vscode-json-languageserver";
       };
 
       language = [
         {
           name = "nix";
-          language-servers = ["nil"];
+          language-servers = [ "nil" ];
           auto-format = true;
           formatter.command = "${pkgs.nixfmt-rfc-style}/bin/nixfmt";
         }
 
         {
           name = "python";
-          language-servers = ["pyright"];
+          language-servers = [ "pyright" ];
           auto-format = true;
           formatter = {
             command = "${pkgs.black}/bin/black";
-            args = ["--quiet" "-"];
+            args = [
+              "--quiet"
+              "-"
+            ];
+          };
+        }
+        {
+          name = "json";
+          formatter = {
+            command = "${pkgs.nodePackages.prettier}/bin/prettier";
+            args = [
+              "--parser"
+              "json"
+            ];
           };
         }
       ];
     };
-    settings = {};
+    settings = { };
   };
 }
